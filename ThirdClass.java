22938999850eddba68bf4977842fee6bd160628e
davidleha <davidleha@ukr.net>
2017-02-04 21:14:41 +0200

    дз1

---
+++ ThirdClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by davidleha on 04.02.17.
+ */
+public class ThirdClass {
+    public static void main(String args[]){
+        System.out.println("Мой первый класс на Java");
+    }
+}
