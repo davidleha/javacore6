4a529930275ff19e4c29cd1188d7210e5a09f25a
davidleha <davidleha@ukr.net>
2017-02-25 19:48:44 +0200

    изм. от 25 февр.

--- module3/Arithmetic.java
+++ module3/Arithmetic.java
@@ -2,12 +2,12 @@ package module3;
 
 public class Arithmetic {
     public static int add(Integer a, Integer b) {
-        return a.intValue() + b.intValue();
+        return a + b;
     }
 
     public static void main(String[] args) {
-        Integer a = new Integer(5);
-        Integer b = new Integer(6);
+        Integer a = 5;
+        Integer b = 6;
         System.out.println(Adder.check(a, b));
         System.out.println(Adder.add(a, b));
     }
@@ -15,6 +15,6 @@ public class Arithmetic {
 
 class Adder extends Arithmetic {
     public static boolean check(Integer a, Integer b) {
-        return a.intValue() >=  b.intValue();
+        return a >=  b;
     }
 }
